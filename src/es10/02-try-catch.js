d430434481c610ee53759e4196459e824288387f
jhon.lerma <john.enfermeria3645@gmail.com>
2022-12-18 11:11:43 -0500

    ya voy por ecmascript 11

--- src/es10/02-try-catch.js
+++ src/es10/02-try-catch.js
@@ -1,5 +1,11 @@
 try {
     hello();
 } catch (error) {
-    console.error(`%c ${error}`, 'background: #222; color: #bada55');
+    console.error(`%c ${error}`, 'background: #f00; color: #bada55');
+}
+
+try {
+    hello();
+} catch (error) {
+    console.error(`%c Ocurrio un error(${error})`, 'background: #f00; color: #bada55');
 }
